9cf3eb7f83cc314b1bbcf53c11b911d78aa37c65
Lucy Park <ej@lucypark.kr>
2013-02-04 05:50:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# UCI Machine Learning Repository Dataset List
+
+## Overview
+The list of datasets in the [UCI Machine Learning Repository](http://archive.ics.uci.edu/ml/datasets.html) in TSV(Tab Separated Values) format.
+
+## Description
+- 235 datasets in total (Feb 4th, 2013)
+- [Citation Policy](http://archive.ics.uci.edu/ml/citation_policy.html)
---
+++ uci.py
@@ -0,0 +1,67 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import html5lib, json
+
+import encoding
+
+#TODO : data id 부여
+
+def read(url):
+    with open(url, 'r') as f:
+        parser = html5lib.HTMLParser(\
+                tree=html5lib.treebuilders.getTreeBuilder("lxml"),\
+                namespaceHTMLElements=False)
+        root = parser.parse(f)
+    return root
+
+def parse(table, stops):
+    rows = []
+    for row in table[1::2]:
+        columns = list(row.itertext())[2::2]
+        columns = columns[0:1] + [columns[1].replace('\n','')] + columns[2:]
+        columns = [sanitize(item, stops) for item in columns]
+        rows.append(columns)
+    return rows
+
+def sanitize(item, stops):
+    item = encoding.to_unicode_or_bust(item)
+    for s in stops:
+        item = item.replace(s, '')
+    item = item.strip()
+    #item = item.replace(', ','|')
+    return item
+
+def concat(rows, baseurl, urlhash):
+    return [row + ["%s%s" % (baseurl, urlhash[row[0]])] for row in rows]
+
+def write(rows, attrs, outp):
+    with open(outp, 'w') as f:
+        f.write('\t'.join(attrs + ['\n']))
+        i = 1
+        for row in rows:
+            row = '\t'.join([str(i)] + row)
+            f.write(row.encode('utf-8'))
+            f.write('\n')
+            i += 1
+
+def main():
+    URL = 'datasets.html'
+    OUTP = 'datasets.tsv'
+    STOPS = [u'\n', u'\t', u' <td><p class="normal">', u'</p></td> ', u'&nbsp;']
+    ATTRS = ['id', 'name', 'abstract', 'data_type', 'default_task', 'attribute_type',
+            'n_instances', 'n_attrs', 'y', 'area', 'url']
+    BASEURL = 'http://archive.ics.uci.edu/ml/'
+
+    root = read(URL)
+    table = root.xpath('//table[@border="1"]//tr')
+    names = root.xpath('//p[@class="normal"]/b/a/text()')
+    urls = root.xpath('//p[@class="normal"]/b/a/@href')
+    urlhash = dict(zip(names, urls))
+
+    rows = parse(table, STOPS)
+    rows = concat(rows, BASEURL, urlhash)
+    write(rows, ATTRS, OUTP)
+
+main()
